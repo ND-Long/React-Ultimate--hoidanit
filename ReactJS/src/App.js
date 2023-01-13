a1e9b4255d4c486ce2cccc2b4af1a43b954099ed
Long <nguyncl622001@gmail.com>
2023-01-13 14:21:01 +0700

    video #31 - delete data witd FILTER

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -2,22 +2,59 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import { useEffect, useState } from 'react';
+
+
+let arr = [
+  {
+    name: "Long",
+    id: 0
+  },
+  {
+    name: "Tùng",
+    id: 1
+  },
+  {
+    name: "Quang",
+    id: 2
+  },
+  {
+    name: "Minh",
+    id: 3
+  },
+
+]
+
 const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+  const [courses, setCourses] = useState(arr)
+  const handelDelete = (id) => {
+    let coursesClone = [...courses]
+    console.log("Chua xoa: ", coursesClone)
+    coursesClone = coursesClone.filter(item => item.id !== id)
+    setCourses(coursesClone)
+    console.log("Da Xoa: ", coursesClone)
+  }
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
+      <label>Name</label>
+      <div>
+        {courses.map((course) => {
+          return (
+            <div>
+              <div key={course.id}>
+                {course.name}
+                <br />
+                <button onClick={() => handelDelete(course.id)}>Delete</button>
+                <hr />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div >
   );
 }
 
